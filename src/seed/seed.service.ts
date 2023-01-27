086a06a8ced52f4746279a6f14d4c43193dd1fa1
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2023-01-26 21:53:11 -0600

    delete all products on seed

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
+  async execute() {
+    await this.insertNewProducts();
+    return 'seed executed!';
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts();
+
+    return true;
+  }
+}
