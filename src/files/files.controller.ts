39472f926793d1086344023cc29ec06eec2f04a1
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2023-01-28 20:28:18 -0600

    return secure url for new images uploaded

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -5,15 +5,23 @@ import {
   UseInterceptors,
   ParseFilePipe,
   FileTypeValidator,
+  Get,
+  Param,
+  Res,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileNamer } from './helpers/file-namer';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('product')
   @UseInterceptors(
@@ -34,8 +42,20 @@ export class FilesController {
     )
     file: Express.Multer.File,
   ) {
-    console.log({ file });
+    // const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
 
-    return { filename: file.originalname };
+    return { secureUrl };
+  }
+
+  @Get('/product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getImagePath(imageName);
+    res.sendFile(path);
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { join } from 'path';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { existsSync } from 'fs';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  getImagePath(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+
+    if (!existsSync(path)) {
+      throw new NotFoundException(`No image found with image: ${imageName}`);
+    }
+    return path;
+  }
+}
